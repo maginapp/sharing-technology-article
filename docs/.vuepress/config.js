d435179c9cfd9d14b8877501e03fa656c0445aef
magina <1075433743@qq.com>
2020-05-10 00:19:26 +0800

    feat: 添加指导页面 test: 发布版本为上次版本，测试此问题

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,5 +8,40 @@ module.exports = {
     // dest: 'public',
     base: `/${basePath}/`,
     serviceWorker: true,
-    evergreen: true
+    evergreen: true,
+    themeConfig: {
+        nav: [
+          { text: 'Home', link: '/' },
+          { text: 'Guide', link: '/guide/' },
+        //   {
+        //     text: 'Languages',
+        //     items: [
+        //       { text: 'Chinese', link: '/language/chinese/' },
+        //       { text: 'English', link: '/language/english' }
+        //     ]
+        //   },
+          { text: 'External', link: 'https://www.magina.top' },
+        ],
+        // displayAllHeaders: true,
+        sidebar: [
+          {
+            title: 'HELP',   // 必要的
+            collapsable: true, // 可选的, 默认值是 true,
+            sidebarDepth: 2,    // 可选的, 默认值是 1
+            children: [
+            //   '/foo/one',
+            //   '/foo/'
+            ]
+          },
+          {
+            title: 'HOME',   // 必要的
+            path: '/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+            collapsable: false, // 可选的, 默认值是 true,
+            sidebarDepth: 2,    // 可选的, 默认值是 1
+            children: [
+              '/'
+            ]
+          },
+        ]
+      },
 }
