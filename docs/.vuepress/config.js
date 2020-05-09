9c7a8c421095d400ab0946967bc4f9f45c4962b7
magina <1075433743@qq.com>
2020-05-09 17:04:31 +0800

    chore: change config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: '我的博客', 
     description: '万事皆虚，万事皆允',
     // vuepress config
-    dest: basePath,
+    dest: 'public',
     base: `/${basePath}/`,
     serviceWorker: true,
     evergreen: true
