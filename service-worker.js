5bcbfca5477edf8aa104ea5d9bce9dd7bf74a3b4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-01 14:06:18 +0000

    Deploy maginapp/sharing-technology-article to github.com/maginapp/sharing-technology-article.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -27,7 +27,7 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "5f94adf362959259ac03ae7f492486de"
+    "revision": "14f466d7de3bf9b8cb8af1c06b01c516"
   },
   {
     "url": "assets/css/0.styles.88f2ffdb.css",
@@ -286,8 +286,8 @@ self.__precacheManifest = [
     "revision": "ab124798a931a56406afaae0211b041b"
   },
   {
-    "url": "assets/js/21.6b4a2b32.js",
-    "revision": "e4e55c912ecee40d2976b1e8032518a1"
+    "url": "assets/js/21.e95c7f64.js",
+    "revision": "9854d17ab5551ad0dabc39d4421fabe0"
   },
   {
     "url": "assets/js/22.22a319f2.js",
@@ -354,8 +354,8 @@ self.__precacheManifest = [
     "revision": "b2faf4ee06fe00e9fecb1efef4fd65f9"
   },
   {
-    "url": "assets/js/app.7d5426b0.js",
-    "revision": "0d51ac7d7451de5b3e4ae4cfe56f5b0e"
+    "url": "assets/js/app.dcfc3e19.js",
+    "revision": "8bd1018e85e0482995f43867125c47ed"
   },
   {
     "url": "assets/js/vendors~flowchart.debf4c03.js",
@@ -363,83 +363,83 @@ self.__precacheManifest = [
   },
   {
     "url": "audition/css.html",
-    "revision": "a2966f803762f42a4396192c5520e50a"
+    "revision": "eeb412067118906c6f393e0ca2942333"
   },
   {
     "url": "audition/history.html",
-    "revision": "c4a0611ec526574bea41e5032cb6fd97"
+    "revision": "96c0720714b3d36f4155efbeb0384247"
   },
   {
     "url": "audition/html.html",
-    "revision": "d76101d25534a18095e40dadd0ecb2bc"
+    "revision": "b572940b487e4d17fe1891ecb6d6f2c0"
   },
   {
     "url": "audition/index.html",
-    "revision": "423d4a00b25bd75ac8803cf112dbfd5f"
+    "revision": "ca078f4a79663621cda8231f1a5916a3"
   },
   {
     "url": "audition/js.html",
-    "revision": "c809aa0ac1d9518c5f48c7aabf9dbed0"
+    "revision": "c9ceb2f927dff9d9e86db9a0e1d85346"
   },
   {
     "url": "audition/nodejs.html",
-    "revision": "87ed0d090712ede9f96b90c23e958118"
+    "revision": "932765e39caf205dc4f78614d700a261"
   },
   {
     "url": "audition/react.html",
-    "revision": "04d392379c1fd8fe52213f79d8eaf6b8"
+    "revision": "77b03674f3e60ea68dffd3a22591b04b"
   },
   {
     "url": "audition/vue.html",
-    "revision": "b638bf8e0bfbe3da7fbb6266e2da03a6"
+    "revision": "5079cc129f91766407597ade1cf96ee4"
   },
   {
     "url": "audition/webpack.html",
-    "revision": "0ad5c1979df7e70e697ff94503a50f43"
+    "revision": "3d6167bcc1d6206cfa1b658d1a4cae2e"
   },
   {
     "url": "audition/小程序.html",
-    "revision": "89091f70a32af8644970861dd09aadae"
+    "revision": "e915daf7e2b05dc2a638fc198ead601d"
   },
   {
     "url": "blog/about-config/vuepress搭建网站踩坑.html",
-    "revision": "2060f81c436b9d42f15644c42a2e6113"
+    "revision": "824600a82f00a6ad97b4930965adefee"
   },
   {
     "url": "blog/html-test/测试页面汇总.html",
-    "revision": "b90db84a98c744f8346b11306fc0f87b"
+    "revision": "0ee0a98fcfa5c7d78651646591db7e59"
   },
   {
     "url": "blog/html-test/锚点功能测试.html",
-    "revision": "1c66671e780458e1b65eebfec2a94105"
+    "revision": "bf108594e26f807117b70258ab84e2e7"
   },
   {
     "url": "blog/index.html",
-    "revision": "0e6f235af5a4ed1c5ea57a3e4aca687b"
+    "revision": "e95e3163425d2b0bbe972b9252b352ec"
   },
   {
     "url": "blog/standard/git-commit.html",
-    "revision": "ceb1796a3a32b3ee808c898e233d6067"
+    "revision": "49342438ed215fa427e21b909bb1497d"
   },
   {
     "url": "blog/standard/index.html",
-    "revision": "cc7cbbbc019abcf31e1970581eaf2f7a"
+    "revision": "cb14c1db23489bd657acd2a7ed80c1ec"
   },
   {
     "url": "blog/standard/internet.html",
-    "revision": "22b4f43cd913351c99afa20ae25f5323"
+    "revision": "6f8d49e0e8b92c2f2880d0f2b59b657d"
   },
   {
     "url": "Goal.html",
-    "revision": "b6ef3f0b94d09d75d7d0b20590712046"
+    "revision": "1057acaa1f4d54dabfd54328a68c5fc8"
   },
   {
     "url": "index.html",
-    "revision": "dbc83dc28390192fb83e37780952c001"
+    "revision": "5d1d5ae541a8e1e3ddc7bb3f5b5a4f1e"
   },
   {
     "url": "sharing/index.html",
-    "revision": "885708306172eb305d44d1210bd507b2"
+    "revision": "06d37e516c667af0f70f03f0bae9650a"
   },
   {
     "url": "test.png",
