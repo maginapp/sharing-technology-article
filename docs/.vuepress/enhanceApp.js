e13c655f19db94dd7bd39940ababe240363487f7
magina <1075433743@qq.com>
2021-04-24 18:31:17 +0800

    fix: 锚点滚动功能/处理错误锚点

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -15,7 +15,7 @@ export default (context) => {
 				document.body.addEventListener('click', (e) => {
 					if (e.target) {
 						const node = e.target
-						if (node.href) return
+						if (!node.href) return
 						let url
 						try {
 							url = new URL(node.href)
@@ -25,12 +25,17 @@ export default (context) => {
 						if (node.nodeName === 'A' && url.hash) {
 							const local = new URL(location.href)
 							if (local.pathname === url.pathname) {
-								const id = decodeURIComponent(url.hash.substring(1))
+								const id = solveHansh(decodeURIComponent(url.hash.substring(1)))
 								const element = document.getElementById(id)
 								if (element) {
 									element.scrollIntoView({behavior: "smooth"})
 									e.stopPropagation()
 									e.preventDefault()
+									setTimeout(() => {
+										context.router.app.$router.push({
+											hash: '#' + id
+										})
+									}, 500)
 								}
 							}
 						}
@@ -45,4 +50,16 @@ export default (context) => {
 		// 	next()
 		// })
 	}
+}
+
+// 基于当前发现的规则进行处理，没有查看md编译代码确认规则
+// &nbsp; 11sd111 1?ds!@@$)* => _11sd111-1-ds
+//   __11__---1-23___3s---dsa---3-sd##__ => _11-1-23-3s-dsa-3-sd
+// ---dsd2 => dsd2
+// __dsd--asd--dsd2 ==> dsd-asd-dsd2
+// fix: vuepress编译时，id的大写字母变小写，重复的添加order, 空格转换等
+function solveHansh (hash = '') {
+	// [u4e00-u9fa5]
+	hash = hash.replace(/([^\u4e00-\u9fa50-9a-zA-Z-]+)/g, '-').replace(/[-]+/g, '-').replace(/(^-*)|(-*$)/g, '').replace(/^([0-9])/, '_$1')
+	return hash.toLocaleLowerCase()
 }
